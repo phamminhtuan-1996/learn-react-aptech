e54b00611da86edd87c0b011f78da378d8facb88
pmtuan <phamminhtuan02011996@gmail.com>
2024-10-08 20:48:29 +0700

    hoc ve component

---
+++ src/AptechComponent.jsx
@@ -0,0 +1,10 @@
+import './aptech.css';
+function AptechComponent() {
+    return (
+        <div>
+        <h1 className="red">Aptech</h1>
+        </div>
+    )
+}
+
+export default AptechComponent;
