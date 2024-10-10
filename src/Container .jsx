f0f2abe2c25633a2e54b360fe0941752d7751d79
pmtuan <phamminhtuan02011996@gmail.com>
2024-10-10 21:00:15 +0700

    học về props

--- src/Container .jsx
+++ src/Container .jsx
@@ -4,11 +4,30 @@ import Center from './Center'
 import Right from './Right'
 
 function Container() {
+    const listItem = [
+        'item1',
+        'item2',
+        'item3',
+        'item4',
+        'item5',
+    ]
     return (
         <div className="container">
-            <Left/>
-            <Center/>
-            <Right/>
+            <Left data={listItem}>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus nam, quis, autem commodi quidem dolorem veritatis suscipit possimus repellat natus tempore, illum dignissimos aperiam! Expedita explicabo cumque et. Possimus, similique?
+                </p>
+            </Left>
+            <Left name="Tèo" age={29}  phone="0798208715">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus nam, quis, autem commodi quidem dolorem veritatis suscipit possimus repellat natus tempore, illum dignissimos aperiam! Expedita explicabo cumque et. Possimus, similique?
+                </p>
+            </Left>
+            <Left name="Tí" age={30}  phone="0798208716" >
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus nam, quis, autem commodi quidem dolorem veritatis suscipit possimus repellat natus tempore, illum dignissimos aperiam! Expedita explicabo cumque et. Possimus, similique?
+                </p>
+            </Left>
         </div>
     )
 }
--- src/Left.jsx
+++ src/Left.jsx
@@ -1,8 +1,37 @@
-function Left() {
+import PropType, { array } from 'prop-types';
+
+Left.propType = {
+    name: PropType.string, 
+    age: PropType.number, 
+    phone: PropType.string,
+    data: PropType.array, 
+    children: PropType.node
+}
+
+Left.defaultProps = {
+    name: "Tuan",
+    age: 28,
+    phone: '0798208714',
+    array: []
+}
+
+
+function Left({name, age, phone, children, data}) {
     return (
-        <div className="left">
-            <h1>Left</h1>
-        </div>
+            <div className="left">
+                <h1>Hello {name}</h1>
+                <h1>age: {age}</h1>
+                <h1>phone: {phone}</h1>
+                {children}
+                <ul>
+                {
+                   data && data.map((item, index) => (
+                        <li key={index}> {item} </li>
+                    ))
+                }
+            </ul>
+            </div>
+            
     )
 }
 
