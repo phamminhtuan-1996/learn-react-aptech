3805bb81978b2f85e8402ae0ea3a6542fca56aaf
pmtuan <phamminhtuan02011996@gmail.com>
2024-10-24 21:02:05 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,116 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Table from 'react-bootstrap/Table';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 function App() {
-  const [count, setCount] = useState(0)
+  const [val, setVal] = useState('');
+  const [category, setCategory] = useState([]);
+  const [isEdit, setEdit] = useState(false);
+  const getCategory = () => {
+    fetch('https://671a4236acf9aa94f6a9e048.mockapi.io/category').then((res) => res.json()).then((res) => {
+      console.log(res);
+      setCategory(res);
+    })
+  } 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('handleSubmit');
+    fetch('https://671a4236acf9aa94f6a9e048.mockapi.io/category', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name: val})
+    }).then((res) => {
+      console.log(res);
+      setVal('');
+      getCategory();
+    })
+  }
 
+  const removeItem = (id) => {
+    fetch('https://671a4236acf9aa94f6a9e048.mockapi.io/category/'+id, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then((res) => {
+      console.log(res);
+      getCategory();
+    })
+  }
+  const setValEdit = (value, index) => {
+    setCategory(category.map((item, indexs) => indexs === index ? {...item, name: value} : item));
+    
+  }
+  const handleSubmitSave = (id) => {
+    console.log(id);
+    const findValue = category.find((item) => item.id === id);
+    fetch('https://671a4236acf9aa94f6a9e048.mockapi.io/category/' + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name: findValue})
+    }).then((res) => {
+      console.log(res);
+      setEdit(false);
+      getCategory();
+    })
+  }
+
+  useEffect(() => {
+    getCategory();
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <Table striped bordered hover>
+       <thead>
+        <tr>
+          <th colSpan={3}>
+          <form onSubmit={handleSubmit} className='d-flex'>
+          <Form.Control type="text" value={val} placeholder='input category'onChange={(e) => setVal(e.target.value)}/> 
+            <Button variant='info' className='text-white' onClick={() => {setEdit(!isEdit)}}>Edit</Button>
+          </form>
+          </th>
+        </tr>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Acction</th>
+        </tr>
+      </thead>
+        <tbody>
+        {category.map((item, index) => (
+          <tr key={index}>
+            <td>{item.id}</td>
+            <td>
+            {!isEdit && (
+              <>
+                {item.name}
+              </>
+            )}
+            {isEdit && (
+              <>
+                <Form.Control type="text" value={item.name} placeholder='input category' onChange={(e) => setValEdit(e.target.value, index)}/> 
+              </>
+            )}
+            </td>
+            <td>
+              {isEdit && (
+                <Button variant='success' onClick={()=> handleSubmitSave(item.id)}>Save</Button>
+              )}
+              <Button variant="danger" onClick={() => removeItem(item.id)}>Remove</Button> 
+            </td>
+            
+          </tr>
+        ))}
+          
+          </tbody>
+       </Table>
     </>
   )
 }
